4ea5ee78b32f0ccfc88e6741675cda4ecbd78af7
Rory Magee <rory.magee2@gmail.com>
2019-01-23 22:42:00 +0000

    added carousel

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+new Glider(document.querySelector('.glider'), {
+    slidesToShow: 3,
+    draggable: true,
+    dots: '#dots',
+    arrows: {
+        prev: '.glider-prev',
+        next: '.glider-next'
+    }
+});
