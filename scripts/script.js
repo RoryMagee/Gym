0974a3fa2d9ec1c9f492205657b848825f4e36a5
Rory Magee <rory.magee2@gmail.com>
2019-01-25 20:01:09 +0000

    personal training section

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,5 @@ new Glider(document.querySelector('.glider'), {
     slidesToShow: 3,
     draggable: true,
     dots: '#dots',
-    arrows: {
-        prev: '#glider-prev',
-        next: '#glider-next'
-    }
+    dragVelocity: 2
 });
